8314f5a59bd053323f525f560ad5263f75a54743
Natalia Tomys <hanf.natalia@gmail.com>
2020-11-09 16:47:25 +0100

    RWD final version

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-function myFunction() {
+function myFunction(event) {
+  event.preventDefault();
   const x = document.getElementById("links");
   if (x.style.display === "block") {
     x.style.display = "none";
@@ -6,15 +7,6 @@ function myFunction() {
     x.style.display = "block";
   }
 }
-
-const linksWithSublinks = document.querySelectorAll('.sublinked');
-console.log(linksWithSublinks);
-
-for (const link of linksWithSublinks) {
-  link.addEventListener('click', sublinksFunctionHandler);
-  console.log(link);
-}
-
 
 function sublinksFunctionHandler(event) {
   event.preventDefault();
@@ -29,5 +21,15 @@ function sublinksFunctionHandler(event) {
   } else {
     subMenu.style.display = "block";
   }
+}
+
+const linksWithSublinks = document.querySelectorAll('.sublinked');
+const burgerMenu = document.querySelector('.mobile-nav');
+
+burgerMenu.addEventListener('click', myFunction)
+
+for (const link of linksWithSublinks) {
+  link.addEventListener('click', sublinksFunctionHandler);
+  console.log(link);
 }
 
